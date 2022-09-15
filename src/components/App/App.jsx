6ba336b24a4768b34e3e27b02ee7ac05d2009808
Maxxbay <maxxbay@gmail.com>
2022-09-15 20:38:03 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -22,38 +22,35 @@ const App = () => {
     console.log(error);
   }
 
-  useEffect(
-    prevQuery => {
-      if (query !== '' || query === prevQuery) {
-        setIsLoading(prevIsLoading => !prevIsLoading);
-        fetchImages(query, page)
-          .then(({ hits, totalHits }) => {
-            const imagesArray = hits.map(hit => ({
-              id: hit.id,
-              description: hit.tags,
-              smallImage: hit.webformatURL,
-              largeImage: hit.largeImageURL,
-            }));
-            setTotalImages(totalHits);
-            return imagesArray;
-          })
-          .then(imagesArray => {
-            if (page === 1) {
-              setImages(imagesArray);
-            }
-            return imagesArray;
-          })
-          .then(imagesArray => {
-            if (page !== 1) {
-              setImages(prevImages => [...prevImages, ...imagesArray]);
-            }
-          })
-          .catch(error => setError(error))
-          .finally(() => setIsLoading(prevIsLoading => !prevIsLoading));
-      }
-    },
-    [page, query]
-  );
+  useEffect(() => {
+    if (query !== '') {
+      setIsLoading(prevIsLoading => !prevIsLoading);
+      fetchImages(query, page)
+        .then(({ hits, totalHits }) => {
+          const imagesArray = hits.map(hit => ({
+            id: hit.id,
+            description: hit.tags,
+            smallImage: hit.webformatURL,
+            largeImage: hit.largeImageURL,
+          }));
+          setTotalImages(totalHits);
+          return imagesArray;
+        })
+        .then(imagesArray => {
+          if (page === 1) {
+            setImages(imagesArray);
+          }
+          return imagesArray;
+        })
+        .then(imagesArray => {
+          if (page !== 1) {
+            setImages(prevImages => [...prevImages, ...imagesArray]);
+          }
+        })
+        .catch(error => setError(error))
+        .finally(() => setIsLoading(prevIsLoading => !prevIsLoading));
+    }
+  }, [page, query]);
 
   const getSearchRequest = query => {
     setQuery(query);
